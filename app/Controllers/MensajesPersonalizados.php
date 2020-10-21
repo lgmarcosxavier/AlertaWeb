0f2b9ec884fc49a7f2d4e97e8fa20536381f9063
Medardo Flores <49200182+MedardoFlores@users.noreply.github.com>
2020-10-20 23:55:34 -0600

    Cambios Medardo

--- app/Controllers/MensajesPersonalizados.php
+++ app/Controllers/MensajesPersonalizados.php
@@ -9,7 +9,32 @@ class MensajesPersonalizados extends Controller
 {
 	public function index()
 	{
-		
+		$data['title'] = 'Sistema Alerta';
+        $data['page'] = 'departamento';
+
+        $model = new MensajePersonalizadoModel();
+        $modelUsuario = new UsuarioModel();
+        $mensajesResultado = array();
+
+        $mensajes = $model->where('estado', 1)->findAll();
+
+        foreach($mensajes as $mensaje){
+            $item = $mensaje;
+
+            $usuario = $modelUsuario->find($mensaje['id_usuario']);
+            if ( $usuario ){
+                $item['nombreUsuario'] = $usuario['nombre'] . ' ' . $usuario['apellido'];
+            }else{
+                $item['nombreUsuario'] = '';
+            }
+
+            $mensajesResultado[] = $item;
+        }
+
+        $data['mensajesPersonalizados'] = $mensajesResultado;
+
+        return view('pages/mensajesPersonalizados/index', $data);
+
 	}
 
 	//--------------------------------------------------------------------
